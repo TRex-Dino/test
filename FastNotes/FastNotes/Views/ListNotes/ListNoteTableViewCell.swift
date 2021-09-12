161ba88ab2c7facbe65745c373e106f76ac543ef
Dmitry <dmitrysinkov6@gmail.com>
2021-09-12 12:57:49 +0300

    Add files via upload

---
+++ FastNotes/FastNotes/Views/ListNotes/ListNoteTableViewCell.swift
@@ -0,0 +1,20 @@
+//
+//  ListNoteTableViewCell.swift
+//
+//  Created by Dmitry on 28.08.2021.
+//
+
+import UIKit
+
+class ListNoteTableViewCell: UITableViewCell {
+
+    static let identifier = "ListNoteTableViewCell"
+    
+    @IBOutlet weak private var titleLbl: UILabel!
+    @IBOutlet weak private var descriptionLbl: UILabel!
+    
+    func setup(note: Note) {
+        titleLbl.text = note.title
+        descriptionLbl.text = note.desc
+    }
+}
